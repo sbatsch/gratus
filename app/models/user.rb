64e11cae0ff952b28cef1fa5083a38b66a46e5d4
sbatsch <sbatsch2@gmail.com>
2020-03-18 17:03:50 -0500

    topic breakdown method improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
   has_many :journal_entries
   has_many :prompts, through: :journal_entries
 
+
+
   def gratitude_streak
-    ordered_journal_entries = journal_entries.order(date: :asc)
+    ordered_journal_entries = journal_entries.order(date: :DESC)
     last_date = Time.now.to_date
     if journal_entries.last && journal_entries.last.date == last_date
       count = 1
@@ -16,7 +18,7 @@ class User < ApplicationRecord
 
     ordered_journal_entries.each do |current_entry|
       if last_date == current_entry.date
-      elsif last_date == current_entry.date - 1.day
+      elsif last_date == current_entry.date + 1.day
         count += 1
         last_date = current_entry.date
       else
@@ -26,6 +28,8 @@ class User < ApplicationRecord
 
     return count 
   end
+
+
 
   def journals_completed
     journal_entries.count
@@ -47,6 +51,27 @@ class User < ApplicationRecord
 
   def entries_per_day
     report_days.map {|day| journal_entries.where(date: day).count}
+  end
+
+  def topic_breakdown
+    topics = []
+    journal_entries.each do |journal_entry|
+      topics << journal_entry.prompt.topic
+    end
+    
+    topic_count = {}
+    topics.each do |topic| 
+      if topic_count[topic]
+        topic_count[topic] += 1
+      else
+        topic_count[topic] = 1
+      end 
+    end 
+    topic_array = []
+    topic_count.each do |topic, count|
+      topic_array << {name: topic, y: count}
+    end
+    return topic_array
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -42,3 +42,5 @@
 #               content: "We often get in the habit of focusing on ways to improve ourselves and achieve more. While this way of thinking can help us grow, we can also forget to appreciate the positive qualities we already possess. Think of a skill or personality trait for which you are grateful, jot it, down and say why. Remember: celebrate who you are before shifting your attention to who you want to become.",
 #               topic: 3
 #               )
+
+
