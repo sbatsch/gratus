33412eef90b4b205314fe5ce313b45309354d182
sbatsch <sbatsch2@gmail.com>
2020-03-23 10:28:05 -0500

    added journal entry validations

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,6 +1,10 @@
 class JournalEntry < ApplicationRecord
   belongs_to :user
   belongs_to :prompt
+
+  validates :body, presence: true 
+  validates :gratitude_level, presence: true
+  validates :gratitude_change, presence: true
   
   enum gratitude_change: {
                           no_change: 0,
